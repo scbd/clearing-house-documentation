0b8f5abbad119564181147f73cf69d05710d8d1a
Pramod Jacob Sam <pramod.sam.consultant@cbd.int>
2024-08-27 19:55:25 -0400

    Comment for absolute path

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -41,7 +41,7 @@ export default defineConfig(({ mode }) => {
     vite: {
       resolve: {
         alias: {
-          "@": resolve(__dirname, "../../"),
+          "@": resolve(__dirname, "../../"), //absolute path to root
         },
       },
     },
