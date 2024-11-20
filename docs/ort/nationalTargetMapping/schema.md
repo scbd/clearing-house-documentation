9a9713b0beb2ef756f061a82676a3b0c420bff33
Pramod Jacob Sam <pramod.sam.consultant@cbd.int>
2024-11-19 19:11:39 -0500

    National Target Mapping Documentation

---
+++ docs/ort/nationalTargetMapping/schema.md
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+# Schema for National Target Mapping
+
+This section provides an overview of the schemas related to the National Target Mapping. Each schema details the structure and fields required for the related requests and responses. 
+<br><br>
+
+> [!NOTE]
+> When making an API request using the National Target Mapping schema, it is crucial to include the correct schema identifier. The schema parameter must be set to `"nationalTarget7Mapping"` to ensure that the request is processed correctly and targets the appropriate data structure. Failure to provide the correct schema may result in an error or unexpected behavior in the response.
+
+<!--@include: @/../components/nationalTargetMapping/request-body.md-->
--- routes/ort.ts
+++ routes/ort.ts
@@ -1,92 +1,20 @@
 export const recordTypes = [
   {
-    text: "National-Target-Mapping",
+    text: "National Target Mapping",
     fullName: "National Target Mapping",
     collapsed: true,
     items: [
       {
         text: "Schema",
-        link: "/ort/national-target-mapping/schema",
+        link: "/ort/nationalTargetMapping/schema",
       },
       {
         text: "Public Records",
-        link: "/ort/national-target-mapping/solr",
+        link: "/ort/nationalTargetMapping/solr",
       },
       {
         text: "Get",
-        link: "/ort/national-target-mapping/get",
-      },
-    ],
-  },
-  {
-    text: "NBSap",
-    fullName: "NBSap",
-    collapsed: true,
-    items: [
-      {
-        text: "Public Records",
-        link: "/ort/nbsap/solr",
-      },
-      {
-        text: "Get",
-        link: "/ort/nbsap/get",
-      },
-    ],
-  },
-  {
-    text: "National-Report",
-    fullName: "National Report",
-    collapsed: true,
-    items: [
-      {
-        text: "Schema",
-        link: "/ort/national-report/schema",
-      },
-      {
-        text: "Public Records",
-        link: "/ort/national-report/solr",
-      },
-      {
-        text: "Get",
-        link: "/ort/national-report/get",
-      },
-    ],
-  },
-  {
-    text: "National-Report-Indicator-Data",
-    fullName: "National Report Indicator Data",
-    collapsed: true,
-    items: [
-      {
-        text: "Schema",
-        link: "/ort/national-report-indicator-data/schema",
-      },
-      {
-        text: "Public Records",
-        link: "/ort/national-report-indicator-data/solr",
-      },
-      {
-        text: "Get",
-        link: "/ort/national-report-indicator-data/get",
-      },
-    ],
-  },
-  {
-    text: "National-Report-Binary-Indicator-Data",
-    fullName: "National Report Binary Indicator Data",
-    collapsed: true,
-    items: [
-      {
-        text: "Schema",
-        link: "/ort/national-report-binary-indicator-data/schema",
-      },
-      {
-        text: "Public Records",
-        link: "/ort/national-report-binary-indicator-data/solr",
-      },
-      {
-        text: "Get",
-        link: "/ort/national-report-binary-indicator-data/get",
+        link: "/ort/nationalTargetMapping/get",
       },
     ],
   },
@@ -164,136 +92,4 @@ export default [
     ],
   },
   ...recordTypes,
-  {
-    text: "Thesaurus",
-    collapsed: true,
-    items: [
-      {
-        text: "General",
-        collapsed: true,
-        items: [
-          {
-            text: "Countries",
-            link: "/ort/thesaurus/general/countries",
-          },
-          {
-            text: "Libraries",
-            link: "/ort/thesaurus/general/libraries",
-          },
-          {
-            text: "Regions",
-            link: "/ort/thesaurus/general/regions",
-          },
-          {
-            text: "Other",
-            link: "/ort/thesaurus/general/other",
-          },
-        ],
-      },
-      {
-        text: "National Target",
-        collapsed: true,
-        items: [
-          {
-            text: "GBF Targets",
-            link: "/ort/thesaurus/national-target/gbf-targets",
-          },
-          {
-            text: "GBF Goals",
-            link: "/ort/thesaurus/national-target/gbf-goals",
-          },
-          {
-            text: "GBF Target Considerations",
-            link: "/ort/thesaurus/national-target/gbf-target-considerations",
-          },
-          {
-            text: "GBF Degree of Alignment",
-            link: "/ort/thesaurus/national-target/gbf-degree-of-alignment",
-          },
-          {
-            text: "GBF Headline Indicators",
-            link: "/ort/thesaurus/national-target/gbf-headline-indicators",
-          },
-          {
-            text: "GBF Component Indicators",
-            link: "/ort/thesaurus/national-target/gbf-component-indicators",
-          },
-          {
-            text: "GBF Complementary Indicators",
-            link: "/ort/thesaurus/national-target/gbf-complementary-indicators",
-          },
-        ],
-      },
-      {
-        text: "National Target Mapping",
-        collapsed: true,
-        items: [
-          {
-            text: "GBF Headline Indicators",
-            link: "/ort/thesaurus/national-target-mapping/gbf-headline-indicators",
-          },
-        ],
-      },
-      {
-        text: "NBSap",
-        collapsed: true,
-        items: [
-          {
-            text: "Jurisdictions",
-            link: "/ort/thesaurus/nbsap/ort-jurisdictions",
-          },
-          {
-            text: "Approved Status",
-            link: "/ort/thesaurus/nbsap/approved-status",
-          },
-          {
-            text: "Approving Body",
-            link: "/ort/thesaurus/nbsap/approving-body",
-          },
-          {
-            text: "Report Status",
-            link: "/ort/thesaurus/nbsap/report-status",
-          },
-        ],
-      },
-      {
-        text: "National Report",
-        collapsed: true,
-        items: [
-          {
-            text: "Stakeholders",
-            link: "/ort/thesaurus/national-report/stakeholders",
-          },
-          {
-            text: "Policy Instruments",
-            link: "/ort/thesaurus/national-report/policy-instruments",
-          },
-          {
-            text: "Assessment Progress",
-            link: "/ort/thesaurus/national-report/assessment-progress",
-          },
-          {
-            text: "GBF Goals",
-            link: "/ort/thesaurus/national-report/gbf-goals",
-          },
-          {
-            text: "GBF Headline Indicators",
-            link: "/ort/thesaurus/national-report/gbf-headline-indicators",
-          },
-          {
-            text: "GBF Component Indicators",
-            link: "/ort/thesaurus/national-report/gbf-component-indicators",
-          },
-          {
-            text: "GBF Complementary Indicators",
-            link: "/ort/thesaurus/national-report/gbf-complementary-indicators",
-          },
-          {
-            text: "GBF Binary Indicator",
-            link: "/ort/thesaurus/national-report/gbf-binary-indicator",
-          },
-        ],
-      },
-    ],
-  },
 ];
