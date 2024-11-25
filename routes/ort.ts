a7be3c793fbeb2894d6d8644a2c78301c94b22e1
Pramod Jacob Sam <pramod.sam.consultant@cbd.int>
2024-11-25 18:44:51 -0500

    Added doc for national target mapping

--- routes/ort.ts
+++ routes/ort.ts
@@ -350,6 +350,16 @@ export default [
           },
         ],
       },
+      {
+        text: "National Target Mapping",
+        collapsed: true,
+        items: [
+          {
+            text: "GBF Headline Indicators",
+            link: "/ort/thesaurus/gbf-headline-indicators",
+          },
+        ],
+      },
     ],
   },
 ];
