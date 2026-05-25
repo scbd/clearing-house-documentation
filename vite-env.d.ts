75677326dc5ee2be8faae7b03d3b1f94c0eb0ceb
Blaise Fonseca <blaise.fonseca@un.org>
2026-05-25 15:48:49 -0400

    Move to github actions

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -2,7 +2,6 @@ interface ImportMetaEnv {
   readonly VITE_APP_TITLE?: string;
   readonly VITE_API_URL?: string;
   readonly VITE_CLEARING_HOUSE?: string;
-  readonly VITE_API_URL?: string;
   readonly VITE_ACCOUNTS_HOST_URL?: string;
   readonly VITE_ABS_URL?: string;
   readonly VITE_BCH_URL?: string;
